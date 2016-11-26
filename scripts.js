7bc485861d25141f1b6e99b88a7e98fa12db1477
Colin Arpe <colin.j.arpe@outlook.com>
2016-11-26 08:46:16 -0500

    Add pictures for audio controls

--- scripts.js
+++ scripts.js
@@ -2,6 +2,18 @@ var jukebox = {
 
 }
 
+var player = document.getElementsByTagName("audio")[0];
+var playButton = document.getElementsByClassName("button")[0];
+var pauseButton = document.getElementsByClassName("button")[1];
+var volumeUpButton = document.getElementsByClassName("button")[2];
+var volumeDownButton = document.getElementsByClassName("button")[3];
+
+// playButton.addEventListener ("click", player.play());
+playButton.addEventListener ("click", alert("button works"));
+pauseButton.addEventListener ("click", player.pause());
+volumeUpButton.addEventListener ("click", player.volume += 0.1);
+volumeDownButton.addEventListener ("click", player.volume -= 0.1);
+
 function Playlist (title)	{
 	this.title = title;
 	this.songs = [];
@@ -13,5 +25,8 @@ function Playlist (title)	{
 			}
 			songs[position] = newSong;
 		}
+	}
+	this.shuffle = function ()	{
+		this.songs = this.songs.sort (function () {return 0.5 - Math.random() });
 	}
 }
